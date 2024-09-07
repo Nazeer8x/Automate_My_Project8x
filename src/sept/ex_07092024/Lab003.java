833b7e4d510273cf59a89c1e2611e79569db7c9d
Nazeer <smrrck966@gmail.com>
2024-09-07 23:41:04 +0530

    Main method calling program

---
+++ src/sept/ex_07092024/Lab003.java
@@ -0,0 +1,11 @@
+package sept.ex_07092024;
+
+public class Lab003 {
+
+    public static void main(String[] args) {
+        System.out.println("Automate_My_Project8x");
+        System.out.println(2-2);
+//        System.out.println(2+2);
+
+    }
+}
