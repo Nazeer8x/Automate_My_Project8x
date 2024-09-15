2fb76d67a7de8afb894ae4b73f5cf15852efa7a3
Nazeer <smrrck966@gmail.com>
2024-09-15 11:50:24 +0530

    variable declaring method

---
+++ src/sept/ex_11092024/Lab004_DataType_Variable_01.java
@@ -0,0 +1,13 @@
+package sept.ex_11092024;
+
+public class Lab004_DataType_Variable_01 {
+    public static void main(String[] args) {
+        boolean this_is_variable_name = true;
+        System.out.println(this_is_variable_name);
+    }
+
+
+
+
+
+}
