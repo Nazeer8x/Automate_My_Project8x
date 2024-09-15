573cb09f75f3f3f4539c040ab9383e45379067bd
Nazeer <smrrck966@gmail.com>
2024-09-15 23:32:42 +0530

    calculating numbers

---
+++ src/sept/ex_13092024/Lab010_CalculateNumbers.java
@@ -0,0 +1,13 @@
+package sept.ex_13092024;
+
+public class Lab010_CalculateNumbers {
+    public static void main(String[] args) {
+        double a = 4;
+        double b = 6;
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+        System.out.println(a%b);
+    }
+}
