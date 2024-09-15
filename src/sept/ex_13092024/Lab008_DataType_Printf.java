c25f4632d6f86ba827a8bd432375fc750626835f
Nazeer <smrrck966@gmail.com>
2024-09-15 23:13:04 +0530

    Practice string concept

---
+++ src/sept/ex_13092024/Lab008_DataType_Printf.java
@@ -0,0 +1,32 @@
+package sept.ex_13092024;
+
+public class Lab008_DataType_Printf {
+    public static void main(String[] args) {
+        int num = 100;
+        //%d - Any integer, short, long, byte
+        //%s - string
+        //%c - char
+        //%f - float, double
+        System.out.printf("%d * %d = %d",num,1,num*1); // printf is used for formatting
+        System.out.println();
+        System.out.printf("%d * %d = %d",num,2,num*2);
+        System.out.println();
+        System.out.printf("%d * %d = %d",num,3,num*3);
+        System.out.println();
+        System.out.printf("%d * %d = %d",num,4,num*4);
+        System.out.println();
+        System.out.printf("%d * %d = %d",num,5,num*5);
+        System.out.println();
+        System.out.printf("%d * %d = %d",num,6,num*6);
+        System.out.println();
+        System.out.printf("%d * %d = %d",num,7,num*7);
+        System.out.println();
+        System.out.printf("%d * %d = %d",num,8,num*8);
+        System.out.println();
+        System.out.printf("%d * %d = %d",num,9,num*9);
+        System.out.println();
+        System.out.printf("%d * %d = %d",num,10,num*10);
+
+
+    }
+}
---
+++ src/sept/ex_13092024/Lab008_DataType_String.java
@@ -0,0 +1,11 @@
+package sept.ex_13092024;
+
+import java.sql.SQLOutput;
+
+public class Lab008_DataType_String {
+    public static void main(String[] args) {
+        String fname = "Nazeer";
+        String lname = "Uddin";
+        System.out.printf("your name is %s and last name is %s",fname, lname);
+    }
+}
